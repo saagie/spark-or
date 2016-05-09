6be4d51c8e9f408b9b5785a8b4ffc3411d6aa900
Elise Tellier <elise.tellier@insa-rouen.fr>
2016-05-09 16:53:54 +0200

    Update LinearSystem from devel

--- src/main/scala/org/tropic/sparkor/utils/LinearSystem.scala
+++ src/main/scala/org/tropic/sparkor/utils/LinearSystem.scala
@@ -1,51 +1,25 @@
 package org.tropic.sparkor.utils
 
 import org.apache.spark.mllib.linalg.distributed.RowMatrix
-import org.apache.spark.mllib.linalg.{DenseVector, Matrices, Vector, Vectors}
+import org.apache.spark.mllib.linalg.{Matrices, Vector, Vectors}
 
 object LinearSystem {
 
-  def multiplyVector(colU :Vector, b : Vector ): Double ={
-    var res = 0.0
-    val size = colU.size
-    val colUArray = colU.toArray
-    val bArray = b.toArray
-    for (x<-0 until size){
-      res  =res+ colUArray(x) * bArray(x)
-    }
-    res
-  }
-
-  def multiplyScalar(ub :Array[Double], s : Vector) : Vector = {
-    val size = s.size
-    val sArray = s.toArray
-    val resu = for (x<-0 until size) yield 1/sArray(x) * ub(x)
-    Vectors.dense(resu.toArray)
-  }
-
-  def printTime[R](block: => R): R = {
-    val t0 = System.nanoTime()
-    val result = block
-    val t1 = System.nanoTime()
-    println("Elapsed time: " + ((t1 - t0).toDouble / 1E9).toString + "s")
-    result
-  }
-
-  def rowToTransposedTriplet(row: Vector, rowIndex: Long): Array[(Long, (Long, Double))] = {
+  private def rowToTransposedTriplet(row: Vector, rowIndex: Long): Array[(Long, (Long, Double))] = {
     val indexedRow = row.toArray.zipWithIndex
     indexedRow.map{case (value, colIndex) => (colIndex.toLong, (rowIndex, value))}
   }
-
-  def buildRow(rowWithIndexes: Iterable[(Long, Double)]): Vector = {
+  
+  private def buildRow(rowWithIndexes: Iterable[(Long, Double)]): Vector = {
     val resArr = new Array[Double](rowWithIndexes.size)
     rowWithIndexes.foreach{case (index, value) =>
-        resArr(index.toInt) = value
+      resArr(index.toInt) = value
     }
     Vectors.dense(resArr)
   }
 
   //TODO: delete this function if possible for more efficiency
-  def transposeRowMatrix(m: RowMatrix): RowMatrix = {
+  private def transposeRowMatrix(m: RowMatrix): RowMatrix = {
     val transposedRowsRDD = m.rows.zipWithIndex.map{case (row, rowIndex) => rowToTransposedTriplet(row, rowIndex)}
       .flatMap(x => x) // now we have triplets (newRowIndex, (newColIndex, value))
       .groupByKey
@@ -54,7 +28,13 @@ object LinearSystem {
     new RowMatrix(transposedRowsRDD)
   }
 
-  def solveLinearSystem(A: RowMatrix, b: Vector): Vector = {
+  /**
+    * solve the problem Ax=b where A as a matrix and b as a vector are the parameters and x as a vector is the solution.
+    * @param A features matrix A of the problem
+    * @param b Labels Vector b of the problem
+    * @return x the solution of the problem
+    */
+  def solveLinearSystem(A : RowMatrix, b: Vector): Vector = {
     val svd = A.computeSVD(A.numCols.toInt, computeU = true)
     val matU = svd.U
     val s = svd.s // The singular values are stored in a local dense vector.
@@ -63,6 +43,6 @@ object LinearSystem {
     val bMat = Matrices.dense(b.size, 1, b.toArray)
     val ub = matUtranspose.multiply(bMat).rows.collect
     val z = Vectors.dense((for(i <- ub.indices) yield ub(i)(0) / s(i)).toArray)
-    matV.multiply(z.asInstanceOf[DenseVector])
+    matV.multiply(z)
   }
 }
