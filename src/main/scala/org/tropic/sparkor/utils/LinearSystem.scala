51b0733843d58ff17a95f152eb49acf6d6843767
etienne <etienne.jules@insa-rouen.fr>
2016-05-09 10:05:22 +0200

    add documentation of LinearSystem

--- src/main/scala/org/tropic/sparkor/utils/LinearSystem.scala
+++ src/main/scala/org/tropic/sparkor/utils/LinearSystem.scala
@@ -5,44 +5,21 @@ import org.apache.spark.mllib.linalg.{Matrices, Vector, Vectors}
 
 object LinearSystem {
 
-  def multiplyVector(colU :Vector, b : Vector ): Double ={
-    var res = 0.0
-    val size = colU.size
-    val colUArray = colU.toArray
-    val bArray = b.toArray
-    for (x<-0 to size-1){
-      res  =res+ colUArray(x) * bArray(x)
-    }
-    return res
-  }
 
-  def multiplyScalar(ub :Array[Double], s : Vector) : Vector = {
-    val size = s.size
-    val sArray = s.toArray
-    val resu = for (x<-0 to size-1) yield 1/sArray(x) * ub(x)
-    Vectors.dense(resu.toArray)
-  }
-
-  def printTime[R](block: => R): R = {
-    val t0 = System.nanoTime()
-    val result = block
-    val t1 = System.nanoTime()
-    println("Elapsed time: " + ((t1 - t0).toDouble / 1E9).toString + "s")
-    result
-  }
-  def rowToTransposedTriplet(row: Vector, rowIndex: Long): Array[(Long, (Long, Double))] = {
+  private def rowToTransposedTriplet(row: Vector, rowIndex: Long): Array[(Long, (Long, Double))] = {
     val indexedRow = row.toArray.zipWithIndex
     indexedRow.map{case (value, colIndex) => (colIndex.toLong, (rowIndex, value))}
   }
-  def buildRow(rowWithIndexes: Iterable[(Long, Double)]): Vector = {
+  private def buildRow(rowWithIndexes: Iterable[(Long, Double)]): Vector = {
     val resArr = new Array[Double](rowWithIndexes.size)
     rowWithIndexes.foreach{case (index, value) =>
         resArr(index.toInt) = value
     }
     Vectors.dense(resArr)
   }
+
   //TODO: delete this function if possible for more efficiency
-  def transposeRowMatrix(m: RowMatrix): RowMatrix = {
+  private def transposeRowMatrix(m: RowMatrix): RowMatrix = {
     val transposedRowsRDD = m.rows.zipWithIndex.map{case (row, rowIndex) => rowToTransposedTriplet(row, rowIndex)}
       .flatMap(x => x) // now we have triplets (newRowIndex, (newColIndex, value))
       .groupByKey
@@ -50,6 +27,13 @@ object LinearSystem {
       .map(buildRow) // restore order of elements in each row and remove column indexes
     new RowMatrix(transposedRowsRDD)
   }
+
+  /**
+    * solve the problem Ax=b where A as a matrix and b as a vector are the parameters and x as a vector is the solution.
+    * @param A features matrix A of the problem
+    * @param b Labels Vector b of the problem
+    * @return x the solution of the problem
+    */
   def solveLinearSystem(A : RowMatrix, b: Vector): Vector = {
     val svd = A.computeSVD(A.numCols.toInt, computeU = true)
     val matU = svd.U
