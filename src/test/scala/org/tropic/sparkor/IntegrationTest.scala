e9a5112c546666d755fd2273feb2669b66b411c2
etienne <etienne.jules@insa-rouen.fr>
2016-05-12 15:02:20 +0200

    update IntegrationTest: testing score passed

--- src/test/scala/org/tropic/sparkor/IntegrationTest.scala
+++ src/test/scala/org/tropic/sparkor/IntegrationTest.scala
@@ -1,5 +1,7 @@
 package org.tropic.sparkor
-
+/**
+  * Created by etienne on 12/05/16.
+  */
 
 import org.apache.spark.{SparkConf, SparkContext}
 import org.scalatest._
@@ -8,15 +10,20 @@ import org.apache.spark.{SparkContext, SparkConf}
 import org.tropic.sparkor.core.{Solver, Solution}
 import org.tropic.sparkor.linprog.{ConstraintType, LinearOptimizationProblem, InteriorPointSolver}
 
-/**
-  * Created by etienne on 12/05/16.
-  */
+
 class IntegrationTest extends FlatSpec {
 
   val conf = new SparkConf().setAppName("Tests").setMaster("local[*]")
   val sc = new SparkContext(conf)
   var xExp : Solution = new Solution()
-  val xThe : Vector = Vectors.dense(1,2,8,0,0,2,0)
+  val xThe : Vector = Vectors.dense(1,2)
+  var score : Double = 0
+  val scoreExpected = 70
+  val iterInterval = 1
+  val matA = Matrices.dense(4, 2, Array[Double](8.0, 12.0, 2.0, -1.0, 12.0, 12.0, 1.0, -1.0))
+  val b = Vectors.dense(Array[Double](24, 36, 4, -5))
+  val c = Vectors.dense(Array[Double](30, 20))
+
 
   def solutionFoundCallback(iter: Int, s: Solution, solver: Solver) {
     println("Solution at iteration " + iter)
@@ -31,46 +38,35 @@ class IntegrationTest extends FlatSpec {
     println("Score")
     println(solver.getScore)
     xExp = s
-
+    score = solver.getScore
   }
 
   " test with Interior point solver: A linear optimization problem with A = [8.0, 12.0 ; 2.0, -1.0 ; 12.0, 12.0 ; 1.0, -1.0 ], b = [24 ; 36 ; 4 ; -5] and c = [30, 20] " should " x = [1 ; 2 ; 8 ; 0 ; 0 ; 2 ; 0] " in  {
     val solver = new InteriorPointSolver(sc)
-    val matA = Matrices.dense(4, 2, Array[Double](8.0, 12.0, 2.0, -1.0, 12.0, 12.0, 1.0, -1.0))
-    val b = Vectors.dense(Array[Double](24, 36, 4, -5))
-    val c = Vectors.dense(Array[Double](30, 20))
-    var problem = new LinearOptimizationProblem(matA, b, c, ConstraintType.GreaterThan)
+    val problem = new LinearOptimizationProblem(matA, b, c, ConstraintType.GreaterThan)
     solver.setProblem(problem)
-    val iterInterval = 1
     solver.setNewSolutionFoundCallback(iterInterval,solutionFoundCallback)
     solver.setSolvingStoppedCallback(solvingStoppedCallback)
     solver.solve()
 
     for ( i <- xThe.toArray.indices) {
-      assert( xThe.toArray(i) - xExp.getVector.toArray(i) < 1e-4)
+      assert( (xThe.toArray(i) - xExp.getVector.toArray(i))/xThe.toArray(i) < 1e-4)
     }
-
-
+    val scoreExpected = 70
+    assert((scoreExpected - score)/scoreExpected < 1e-4)
   }
 
 
   " test with generic solver: A linear optimization problem with A = [8.0, 12.0 ; 2.0, -1.0 ; 12.0, 12.0 ; 1.0, -1.0 ], b = [24 ; 36 ; 4 ; -5] and c = [30, 20] " should " x = [1 ; 2 ; 8 ; 0 ; 0 ; 2 ; 0] " in  {
-
-    val matA = Matrices.dense(4, 2, Array[Double](8.0, 12.0, 2.0, -1.0, 12.0, 12.0, 1.0, -1.0))
-    val b = Vectors.dense(Array[Double](24, 36, 4, -5))
-    val c = Vectors.dense(Array[Double](30, 20))
     val problem = new LinearOptimizationProblem(matA, b, c, ConstraintType.GreaterThan)
     val solver = problem.generateDefaultSolver(sc)
-    val iterInterval = 1
     solver.setNewSolutionFoundCallback(iterInterval,solutionFoundCallback)
     solver.setSolvingStoppedCallback(solvingStoppedCallback)
     solver.solve()
-
     for ( i <- xThe.toArray.indices) {
-      assert( xThe.toArray(i) - xExp.getVector.toArray(i) < 1e-4)
+      assert( (xThe.toArray(i) - xExp.getVector.toArray(i))/xThe.toArray(i) < 1e-4)
     }
-
-
+    assert((scoreExpected - score)/scoreExpected < 1e-4)
   }
 
 
